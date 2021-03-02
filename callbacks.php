4a6065afbf999545dcc31ddfed5353f30755dd62
Daniel Gil <danniegil@hotmail.com>
2021-03-01 22:45:26 -0500

    hi

--- callbacks.php
+++ callbacks.php
@@ -1,9 +1,5 @@
 <?php
-echo "test2";
-  // Capturando el objeto JSON
-  $body = @file_get_contents('php://input'); 
-
-  // Decodificando el objeto JSON
-  $event_json = json_decode($body);
-  var_dump($event_json);
+echo "1";
+$data=$_POST["topic"];
+echo "Welcome ". $data. "<br />";
 ?>
